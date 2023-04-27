a3f5b1da9639bda582cb740f5edee179bf3af4eb
rink <rink@arteria-s.net>
2023-04-27 17:57:00 +0900

    update

---
+++ wrk/Etudes/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+
+var pSQL = "SELECT * FROM MAccounts WHERE Name IS NULL;";
+var pMatch = Regex.Match(pSQL, @"[A-Z]*");
+var pIsMatch = Regex.IsMatch(pSQL, @"[A-Z]*");
+
+Console.WriteLine(pMatch);
+Console.WriteLine(pIsMatch);
--- wrk/LookupBookmark/MainWindow.xaml.cs
+++ wrk/LookupBookmark/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Composition.SystemBackdrops;
+﻿using Microsoft.UI.Composition;
+using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,9 +11,11 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -30,10 +33,73 @@ namespace LookupBookmark
 			//TrySetSystemBackdrop();
 		}
 
-		public void Button_Click(object sender, RoutedEventArgs e)
+		public async void Button_Click(object sender, RoutedEventArgs e)
 		{
+			// Create the dialog
+			var messageDialog = new MessageDialog("thumbnail.Name");
+			messageDialog.Commands.Add(new UICommand("Close", new UICommandInvokedHandler(DialogDismissedHandler)));
 
+			MainWindow.ReferenceEquals(this, messageDialog);
+			WinRT.Interop.InitializeWithWindow.Initialize(messageDialog, WinRT.Interop.WindowNative.GetWindowHandle(this));
+
+			// Show the message dialog
+			await messageDialog.ShowAsync();
 		}
+
+		private SpriteVisual _destinationSprite;
+		private Compositor _compositor;
+		private CompositionScopedBatch _scopeBatch;
+
+		private void DialogDismissedHandler(IUICommand command)
+		{
+			// Start a scoped batch so we can register to completion event and hide the destination layer
+			_scopeBatch = _compositor.CreateScopedBatch(CompositionBatchTypes.Animation);
+
+			// Start the hide animation to fade out the destination effect
+			ScalarKeyFrameAnimation hideAnimation = _compositor.CreateScalarKeyFrameAnimation();
+			hideAnimation.InsertKeyFrame(0f, 1f);
+			hideAnimation.InsertKeyFrame(1.0f, 0f);
+			hideAnimation.Duration = TimeSpan.FromMilliseconds(1000);
+			_destinationSprite.StartAnimation("Opacity", hideAnimation);
+
+			// Use whichever effect is currently selected
+					{
+						CompositionSurfaceBrush brush = ((CompositionEffectBrush)_destinationSprite.Brush).GetSourceParameter("SecondSource") as CompositionSurfaceBrush;
+						Vector2KeyFrameAnimation scaleAnimation = _compositor.CreateVector2KeyFrameAnimation();
+						scaleAnimation.InsertKeyFrame(1f, new Vector2(2.0f, 2.0f));
+						scaleAnimation.Duration = TimeSpan.FromMilliseconds(1000);
+						brush.StartAnimation("Scale", scaleAnimation);
+//						break;
+					}
+			/*
+				case EffectTypes.VividLight:
+					{
+						CompositionEffectBrush brush = (CompositionEffectBrush)_destinationSprite.Brush;
+						ColorKeyFrameAnimation coloAnimation = _compositor.CreateColorKeyFrameAnimation();
+						coloAnimation.InsertKeyFrame(1f, Color.FromArgb(255, 100, 100, 100));
+						coloAnimation.Duration = TimeSpan.FromMilliseconds(1500);
+						brush.StartAnimation("Base.Color", coloAnimation);
+						break;
+					}
+				case EffectTypes.Hue:
+					{
+						CompositionEffectBrush brush = (CompositionEffectBrush)_destinationSprite.Brush;
+						ScalarKeyFrameAnimation rotateAnimation = _compositor.CreateScalarKeyFrameAnimation();
+						rotateAnimation.InsertKeyFrame(1f, 0f);
+						rotateAnimation.Duration = TimeSpan.FromMilliseconds(1500);
+						brush.StartAnimation("Hue.Angle", rotateAnimation);
+						break;
+					}
+				default:
+					break;
+			*/
+		}
+		/*
+		//Scoped batch completed event
+		_scopeBatch.Completed += ScopeBatch_Completed;
+			_scopeBatch.End();
+		}
+	*/
 
 		MicaController m_backdropController;
 
