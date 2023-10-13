e8703f91c882e8b1c7f34e0eb301e70bfe66c496
rink <rink@mxd.mesh.ne.jp>
2023-10-13 16:54:57 +0900

    ルート証明書の作成処理を実装

---
+++ wrk/AutoCA/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace AutoCA
+{
+	/// <summary>
+	/// An empty window that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
+		}
+
+		private void createRootCert_Click(object sender, RoutedEventArgs e)
+		{
+			//　署名要求を生成（der形式）
+			string pSubject = "cn=Arteria-RCA";
+			ECDsaCng pKey = new ECDsaCng();
+			CertificateRequest pRequest = new CertificateRequest(pSubject, pKey, HashAlgorithmName.SHA256);
+			//　CA制約：証明書が認証局であるか否かを指定する。
+			pRequest.CertificateExtensions.Add(new X509BasicConstraintsExtension(true, true, 2, true));
+			pRequest.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(pRequest.PublicKey, false));
+			var pKeyUsageFlags = X509KeyUsageFlags.NonRepudiation | X509KeyUsageFlags.KeyCertSign | X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.EncipherOnly;
+			pRequest.CertificateExtensions.Add(new X509KeyUsageExtension(pKeyUsageFlags, false));
+			OidCollection pEnhancedKeyUsageFlags = new OidCollection
+			{
+				// https://oidref.com/1.3.6.1.5.5.7.3.2
+				new Oid("1.3.6.1.5.5.7.3.1"),   //　serverAuth
+				new Oid("1.3.6.1.5.5.7.3.3")    //　codeSigning
+			};
+			pRequest.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(pEnhancedKeyUsageFlags, false));
+
+			var pNotBefore = DateTimeOffset.UtcNow;
+			var pNotAfter = DateTimeOffset.UtcNow.AddDays(365);
+			var pRootCert = pRequest.CreateSelfSigned(pNotBefore, pNotAfter);
+			var pBytes = pRootCert.Export(X509ContentType.Cert);
+
+			File.WriteAllBytes("D:/tmp/root.crt", pBytes);
+		}
+
+		private void DumpCertificate(string pFilepath)
+		{
+			X509Certificate pCert = new X509Certificate(pFilepath);
+			string resultsTrue = pCert.ToString(true);
+			Console.WriteLine(resultsTrue);
+			string resultsFalse = pCert.ToString(false);
+			Console.WriteLine(resultsFalse);
+		}
+
+		private void createCertificate_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void createRequest_Click(object sender, RoutedEventArgs e)
+		{
+			//　署名要求を生成（der形式）
+			string pSubject = "cn=日本語で共通名";
+			ECDsaCng pKey = new ECDsaCng();
+			CertificateRequest	pRequest = new CertificateRequest(pSubject, pKey, HashAlgorithmName.SHA256);
+			//　CA制約：証明書が認証局であるか否かを指定する。
+			pRequest.CertificateExtensions.Add(new X509BasicConstraintsExtension(false, true, 2, true));
+
+			var pBytes = pRequest.CreateSigningRequest();
+
+			File.WriteAllBytes("D:/tmp/certificate.req", pBytes);
+		}
+
+		private void myButton_Click_1(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
